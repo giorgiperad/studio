45d4c9fc84f24bf823a2691b4fd614302860cfca
Giorgi <vasokima@gmail.com>
2026-02-16 10:54:19 +0000

    efect

---
+++ src/app/HomeClient.tsx
@@ -0,0 +1,26 @@
+
+import { Project, Testimonial, BlogPost } from '@/lib/types';
+import dynamic from 'next/dynamic';
+import Hero from '@/components/Hero/Hero';
+import ProjectSection from '@/components/Projects/ProjectSection';
+import TestimonialSection from '@/components/Testimonials/TestimonialSection';
+const BlogSection = dynamic(() => import('@/components/Blogs/BlogSection'), { ssr: false });
+
+interface HomeClientProps {
+  projects: Project[];
+  testimonials: Testimonial[];
+  latestPosts: BlogPost[];
+}
+
+const HomeClient: React.FC<HomeClientProps> = ({ projects, testimonials, latestPosts }) => {
+  return (
+    <main className="mx-auto max-w-[1200px] px-4 md:my-[3.75rem]">
+      <Hero />
+      <ProjectSection projects={projects} />
+      <TestimonialSection testimonials={testimonials} />
+      <BlogSection posts={latestPosts} />
+    </main>
+  );
+};
+
+export default HomeClient;
